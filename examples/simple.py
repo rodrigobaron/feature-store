fbb607d541354f77dddca5d0234ccc2a33257589
Rodrigo Baron <baron.rodrigo0@gmail.com>
2024-04-23 15:59:31 -0300

    update example
    Signed-off-by: Rodrigo Baron <baron.rodrigo0@gmail.com>

--- examples/simple.py
+++ examples/simple.py
@@ -1,46 +1,56 @@
+import qafs
 import pandas as pd
 import pandera as pa
-from pandera import Check, Column, DataFrameSchema, io
+from pandera import Check, Column, DataFrameSchema
+from pandera import io
 
-import qafs
-from qafs import InFeature, OutFeature
 
 fs = qafs.FeatureStore(
-    db_connection='sqlite:///test.sqlite',
-    storage=qafs.LocalStorage(path='/tmp/featurestore/example'),
-    backend='pandas'
+    project='test'
 )
 
-fs.create_namespace('example', description='Example datasets')
+fs.create_namespace('example', description='Example Namespace', partition='date')
+
 fs.create_feature(
     'numbers',
-    namespace="example", 
-    description='Timeseries of numbers', 
+    namespace='example',
+    description='Timeseries of numbers',
     check=Column(pa.Int, Check.greater_than(0))
 )
+
+# fs.create_features(
+#     [
+#         {
+#             'name': 'numbers1',
+#             'description': 'Timeseries of numbers',
+#             'check': Column(pa.Int, Check.greater_than(0))
+#         },
+#         {
+#             'name':'numbers2',
+#             'description': 'Timeseries of numbers',
+#             'check': Column(pa.Int, Check.greater_than(0))
+#         }
+#     ],
+#     namespace='example'
+# )
 
 dts = pd.date_range('2020-01-01', '2021-02-09')
 df = pd.DataFrame({'time': dts, 'numbers': list(range(1, len(dts) + 1))})
 
-fs.save_df(df, name='numbers', namespace='example')
-
+fs.save(df, index='time', features=['numbers'], namespace='example')
 
 @fs.transform(
-    name='squared', 
-    namespace='example', 
-    from_features=[
-        InFeature(name='numbers', namespace='example')
-    ], 
+    'squared',
+    t_namespace="example",
+    from_features=['example/numbers'],
     check=Column(pa.Int, Check.greater_than(0))
 )
 def squared(df):
     return df ** 2
 
 df_query = fs.load_features(
-    features=[
-        OutFeature(name='numbers', namespace='example'),
-    ],
+    ['example/numbers', 'example/squared'],
     from_date='2021-01-01',
     to_date='2021-01-31'
 )
-print(df_query.shape)
+print(df_query.tail(1))
